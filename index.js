76bece9aa8ab7b65a6f9e98849d91990a0f55d38
Aliénor <alienor.vincotte@sciencespo.fr>
2018-11-18 19:03:23 +0100

    Premières étapes du projet

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-var vm = new Vue({
-  el: '.app',
-  data: function () {
-    var now = new Date();
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-
-    return  {
-      now: now,
-      day: days[now.getDay()]
-    };
+var quiz = [
+  {
+    question: "1. Qui sont les quatre sorciers fondateurs de Poudlard ?",
+    choices: ["A. Roselia Serdaigle, Gordius Gryffondor, Holga Poufsouffle, Salazar Serpentard", "B. Rowena Serdaigle, Godric Gryffondor, Helga Poufsouffle, Salazar Serpentard", "C. Rowena Serdaigle, Gidric Gryffondor, Hilge Poufsouffle, Sandor Serpentard"],
+    answer: 1
+  },
+  {
+    question: "2. En quelle année sont morts les parents d'Harry Potter ?",
+    choices: ["A. 1980", "B. 1981", "C. 1982"], 
+    answer: 1
+  },
+  {
+    question: "3. Qu'enseignait Albus Dumbledore avant de devenir directeur de Poudlard ?",
+    choices: ["L'histoire de la magie", "La défense contre les forces du mal", "La métamorphose"],
+    answer: 2
+  },
+  {
+    question: "4. De quelles langues vient la formule 'Alomora' (le sortilège de déverrouillage) ?",
+    choices: ["A. Anglais + Latin", "B. Grec + Latin", "C. Hawaïen + Latin"],
+    answer: 2
+  },
+  {
+    question: "Écris la valeur de la variable resultat. var resultat = 1 * 2 + '1'",
+    answer: "21"
   }
-})
+];
