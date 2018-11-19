3fd8e01cac113a2e6b217aade36987fe225b87db
Aliénor <alienor.vincotte@sciencespo.fr>
2018-11-19 11:50:01 +0100

    Etapes 2

---
+++ quiz.js
@@ -0,0 +1,52 @@
+var quiz = [
+  {
+    question: "1. Qui sont les quatre sorciers fondateurs de Poudlard ?",
+    choices: ["A. Roselia Serdaigle, Gordius Gryffondor, Holga Poufsouffle, Salazar Serpentard", "B. Rowena Serdaigle, Godric Gryffondor, Helga Poufsouffle, Salazar Serpentard", "C. Rowena Serdaigle, Gidric Gryffondor, Hilge Poufsouffle, Sandor Serpentard"],
+    answer: 1
+  },
+  {
+    question: "2. En quelle année sont morts les parents d'Harry Potter ?",
+    choices: ["A. 1980", "B. 1981", "C. 1982"], 
+    answer: 1
+  },
+  {
+    question: "3. Qu'enseignait Albus Dumbledore avant de devenir directeur de Poudlard ?",
+    choices: ["L'histoire de la magie", "La défense contre les forces du mal", "La métamorphose"],
+    answer: 2
+  },
+  {
+    question: "4. De quelles langues vient la formule 'Alomora' (le sortilège de déverrouillage) ?",
+    choices: ["A. Anglais + Latin", "B. Grec + Latin", "C. Hawaïen + Latin"],
+    answer: 2
+  },
+  {
+    question: "5. Lequel de ces mots de passe n'a jamais permis d'ouvrir le bureau d'Albus Dumbledore ?",
+    choices: ["A. Nid de cafard", "B. Eclair au caramal", "C. Chocogrenouille"],
+    answer: 2
+  }, 
+  {
+  	question: "6. Sous quel forme d'animal apparaît le patronus de Ron Weasley ?", 
+  	choices: ["A. Une loutre", "B. Une belette", "C. Un terrier"],
+  	answer: 2
+  }, 
+  {
+  	question: "7. Quels sont les derniers mots de Severus Rogue, dans 'Les Reliques de la Mort'?", 
+  	choices: ["A. 'Pas Harry ! Ayez pitié !'", "B. 'Regardez...moi'", "C. 'Tu peux mieux faire que ça!'"],
+  	answer: 1 
+  }, 
+  {
+  	question: "8. Laquelle de ces propositions n'est pas un Horcruxe dans lequel Voldemort a enfermé une partie de son âme ?", 
+  	choices: ["A. L'épée de Godric Gryffondor", "B. Harry Potter", "C. Le médaillon de Salazar Serpentard"],
+  	answer: 0
+  }, 
+  {
+  	question: "9. Chez Ollivander, la meilleure boutique de baguettes magiques du chemin de Traverse, Harry Potter va trouver sa première baguette. En quoi est-elle ?", 
+  	choices: ["A. En bois de saule et crin de licorne", "B. En bois de houx et plume de phénix", "C. En bois de sureau et crin de sombral"],
+  	answer: 1
+  }, 
+  {
+  	question: "10. Quelle est la devise de Poudlard ?", 
+  	choices: ["A. Draco dormiens nunquam titillandus", "B. Hic sunt dracones", "C. Nolites te bastardes carborundorum"],
+  	answer: 0 
+  }
+];
